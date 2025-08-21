80ab6d22a1bf07f2ffc2e677843eb1dc2a2e48aa
daf01-web <171513@upslp.edu.mx>
2025-08-20 19:27:04 -0600

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,2 +1,10 @@
 -- Template
+CREATE TABLE users (
+    id INTEGRER PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    email VARCHAR NOT NULL
+);
 
+INSERT INTO users (id, name, email) VALUES 
+('Alice', 'alice@example.com'),
+('Bob', 'bob@example.com');
