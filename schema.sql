e6b8a4233fa6f83c6159d8d5ba297e4095c182ab
Jpablo179804 <179804@upslp.edu.mx>
2025-08-20 19:15:08 -0600

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,2 +1,29 @@
--- Template
+CREATE TABLE ESTUDIANTE (
+  id_estudiante SERIAL PRIMARY KEY,
+  nombre VARCHAR(50) NOT NULL,
+  apellido VARCHAR(50) NOT NULL,
+  fecha_nacimiento date NOT NULL
+);
 
+CREATE TABLE CURSO (
+  id_curso SERIAL PRIMARY KEY,
+  nombre_curso VARCHAR(50) NOT NULL,
+  creditos int NOT NULL
+);
+
+CREATE TABLE PROFESOR (
+  id_profesor SERIAL PRIMARY KEY,
+  nombre_profesor VARCHAR(50) NOT NULL,
+  departamento VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE INSCRIPCION (
+  id_estudiante SERIAL REFERENCES estudiante(id_estudiante) NOT NULL,
+  id_curso SERIAL REFERENCES curso(id_curso) NOT NULL,
+  fecha_inscripcion DATE NOT NULL
+);
+
+CREATE TABLE ENSEÑA (
+  id_profesor SERIAL REFERENCES profesor(id_profesor) NOT NULL,
+  id_curso SERIAL REFERENCES curso(id_curso) NOT NULL
+);
