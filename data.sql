319465252f8fea2d39774e13ae4bbbb190e3ec77
ItsMusa17 <160455957+ItsMusa17@users.noreply.github.com>
2025-08-20 19:32:30 -0600

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,3 +1,44 @@
+-- Tabla ESTUDIANTE
+CREATE TABLE ESTUDIANTE (
+  id_estudiante INTEGER PRIMARY KEY,
+  nombre VARCHAR(30) NOT NULL,
+  apellido VARCHAR(30) NOT NULL,
+  fecha_nacimiento DATE NOT NULL
+);
+
+-- Tabla CURSO
+CREATE TABLE CURSO (
+  id_curso INTEGER PRIMARY KEY,
+  nombre_curso VARCHAR(30) NOT NULL,
+  creditos INTEGER NOT NULL
+);
+
+-- Tabla PROFESOR
+CREATE TABLE PROFESOR (
+  id_profesor INTEGER PRIMARY KEY,
+  nombre_profesor VARCHAR(30) NOT NULL,
+  departamento VARCHAR(30) NOT NULL
+);
+
+-- Tabla INSCRIPCION
+CREATE TABLE INSCRIPCION (
+  id_estudiante INTEGER NOT NULL,
+  id_curso INTEGER NOT NULL,
+  fecha_inscripcion DATE NOT NULL,
+  PRIMARY KEY (id_estudiante, id_curso),
+  FOREIGN KEY (id_estudiante) REFERENCES ESTUDIANTE(id_estudiante),
+  FOREIGN KEY (id_curso) REFERENCES CURSO(id_curso)
+);
+
+-- Tabla ENSEÑA
+CREATE TABLE ENSEÑA (
+  id_profesor INTEGER NOT NULL,
+  id_curso INTEGER NOT NULL,
+  PRIMARY KEY (id_profesor, id_curso),
+  FOREIGN KEY (id_profesor) REFERENCES PROFESOR(id_profesor),
+  FOREIGN KEY (id_curso) REFERENCES CURSO(id_curso)
+);
+
 INSERT INTO ESTUDIANTE (nombre, apellido, fecha_nacimiento) VALUES
 ('Juan', 'Pérez', '2000-05-10'),
 ('María', 'González', '2001-02-20'),
@@ -22,3 +63,10 @@ INSERT INTO ENSEÑA (id_profesor, id_curso) VALUES
 (1, 1),
 (2, 2),
 (3, 3);
+
+-- Mostrar los datos insertados
+SELECT * FROM ESTUDIANTE;
+SELECT * FROM CURSO;
+SELECT * FROM PROFESOR;
+SELECT * FROM INSCRIPCION;
+SELECT * FROM ENSEÑA;
