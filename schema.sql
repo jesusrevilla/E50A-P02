efd34c4b701e43ea27cc9cadb84dc31f47dd6a2c
rafaelSanchez4330 <rafaelsanchez9924@gmail.com>
2025-08-20 19:47:56 -0600

    feature/182712

--- schema.sql
+++ schema.sql
@@ -1,2 +1,37 @@
 -- Template
+CREATE TABLE ESTUDIANTE (
+  id_estudiante SERIAL PRIMARY KEY,
+  nombre TEXT NOT NULL,
+  apellido TEXT NOT NULL,
+  fecha_nacimiento DATE NOT NULL
+);
+
+CREATE TABLE CURSO(
+  id_curso SERIAL PRIMARY KEY,
+  nombre_curso TEXT NOT NULL,
+  creditos INTEGER NOT NULL
+);
+
+CREATE TABLE PROFESOR(
+  id_profesor SERIAL PRIMARY KEY,
+  nombre_profesor TEXT NOT NULL,
+  departamento TEXT NOT NULL
+);
+
+CREATE TABLE INSCRIPCION(
+  id_inscripcion SERIAL PRIMARY KEY,
+  id_estudiante INTEGER,
+  id_curso INTEGER,
+  FOREIGN KEY (id_estudiante) REFERENCES ESTUDIANTE(id_estudiante),
+  FOREIGN KEY (id_curso) REFERENCES CURSO(id_curso),
+  fecha_inscripcion DATE NOT NULL
+);
+
+CREATE TABLE ENSEÑA(
+  id_enseña SERIAL PRIMARY KEY,
+  id_profesor INTEGER,
+  id_curso INTEGER,
+  FOREIGN KEY (id_profesor) REFERENCES PROFESOR,
+  FOREIGN KEY (id_curso) REFERENCES CURSO(id_curso)
+);
 
