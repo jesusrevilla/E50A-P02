e2b34fd6412ad009cded347668c1e4e5a663fb93
AdriannMo <165124296+AdriannMo@users.noreply.github.com>
2025-08-20 19:16:42 -0600

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,2 +1,35 @@
--- Template
+CREATE TABLE ESTUDIANTE (
+    id_estudiante SERIAL PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    apellido VARCHAR(100) NOT NULL,
+    fecha_nacimiento DATE NOT NULL
+);
 
+CREATE TABLE CURSO (
+    id_curso SERIAL PRIMARY KEY,
+    nombre_curso VARCHAR(100) NOT NULL,
+    creditos INT NOT NULL
+);
+
+CREATE TABLE PROFESOR (
+    id_profesor SERIAL PRIMARY KEY,
+    nombre_profesor VARCHAR(100) NOT NULL,
+    departamento VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE INSCRIPCION (
+    id_estudiante INT,
+    id_curso INT,
+    fecha_inscripcion DATE NOT NULL,
+    PRIMARY KEY (id_estudiante, id_curso),
+    FOREIGN KEY (id_estudiante) REFERENCES ESTUDIANTE(id_estudiante),
+    FOREIGN KEY (id_curso) REFERENCES CURSO(id_curso)
+);
+
+CREATE TABLE ENSEÑA (
+    id_profesor INT,
+    id_curso INT,
+    PRIMARY KEY (id_profesor, id_curso),
+    FOREIGN KEY (id_profesor) REFERENCES PROFESOR(id_profesor),
+    FOREIGN KEY (id_curso) REFERENCES CURSO(id_curso)
+);
